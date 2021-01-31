bc7068d8d5ea26e58ac14a6aaa9ef2640022df65
master_of_disaster <42957833+childhoodisend@users.noreply.github.com>
2021-01-31 11:50:55 +0300

    Last updates

--- main.cpp
+++ main.cpp
@@ -2,142 +2,61 @@
  * Реализовать функцию, реализующую алгоритм простого скользящего среднего
 */
 
-/*
- * При работе с тестовыми данными -- закоментировать Work Section и раскоментировать Test Section.
- * Для генерации рандомных данных для каждого объекта -- ноборот,
- * закоментировать Test Section и раскоментировать  Work Section.
- */
 #include <iostream>
-
 #include <cstdlib>
 #include <ctime>
-
 #include <vector>
-#include <deque>
-#include <string>
-
-#include "test.cpp"
 
 using namespace std;
 
-
-template<typename T>
-double TRand(T fMin, T fMax) {
-    T f = (T) rand() / RAND_MAX;
-    return fMin + f * (fMax - fMin);
-}
-
-template<typename price_type, typename sum_type>
-class SMA {
-public:
-    ~SMA() = default;
-
-    SMA() = delete;
-
-    explicit SMA(uint8_t _window, price_type _min_price, price_type _max_price) {
-        window = _window;
-        min_price = _min_price;
-        max_price = _max_price;
-        cout << "window -> " << window << endl;
-        /*---Test section---*/
-//        fin_test.open("test.txt");
-//        for (size_t i = 0; i < _window; ++i) {
-//            price_type cur;
-//            fin_test >> cur;
-//            prices.push_back(cur);
-//        }
-        /*~~~Test section~~~*/
-
-        /*---work section---*/
-        for(size_t i = 0; i < _window; ++i)
-          prices.push_back(TRand<price_type>(min_price, max_price));
-        /*~~~work section~~~*/
-
-        for (size_t i = 0; i < window; ++i)
-            sum += prices[i];
-
-        _p.push_back(sum / window);
-    }
-
-    void calculate_sma();
-
-    void save_prices_to_file();
-
-    void save_results_to_file(int clocks, double sec);
-
-private:
-    deque<price_type> prices = {};          // значения исходной функции за установленный период window, храним их c условием FIFO
-    vector<price_type> _p = {};             // храним значения sum/window на каждой итерации
-    price_type min_price = 0, max_price = 0;// диапазон, в котором генерируем цены
-    sum_type sum = 0;                       // храним сумму prices[i] + ... + prices[i + n - 1]
-    uint16_t window = 0;                    // длина окна
-    ofstream fout, fout_price;
-    ifstream fin_test;
-};
-
-
-template<typename price_type, typename sum_type>
-void SMA<price_type, sum_type>::calculate_sma() {
-    if (window > prices.size()) {
-        cerr << "Error" << endl;
-        return;
-    }
-
-
-    for (size_t i = 0; i < 1e6 - 1; ++i) {
-        price_type last_price = prices.front();
-        prices.pop_front();
-
-        /*---Test section---*/
-//        price_type cur;
-//        fin_test >> cur;
-//        prices.push_back(cur);
-        /*~~~Test section~~~*/
-
-        /*---work section---*/
-        prices.push_back(TRand<price_type >(min_price, max_price));
-        /*~~~work section~~~*/
-
-        sum -= last_price;
-        sum += prices.back();
-        _p.push_back(sum / window);
+static const size_t SIGNAL_SIZE = 1e6;
+void print(vector<float> &_p){
+    for(float i : _p){
+        cout << i << endl;
     }
 }
-
-template<typename price_type, typename sum_type>
-void SMA<price_type, sum_type>::save_prices_to_file() {
-    fout_price.open("data" + to_string(window) + typeid(price_type).name() + ".txt");
-    fout_price.clear();
-    for (auto price: _p) {
-        fout_price << price << endl;
-    }
-    fout_price.close();
-}
-
-template<typename price_type, typename sum_type>
-void SMA<price_type, sum_type>::save_results_to_file(int clocks, double sec) {
-    fout.open("results.txt", ios::app);
-    fout << typeid(price_type).name() << " "
-         << typeid(sum_type).name() << " "
-         << window << " "
-         << clocks << " "
-         << sec << endl;
-    fout.close();
-}
-
-
-int main() {
+void fill_random(vector<float> &source_signal){
     srand(time(NULL));
-    //test::test<double>(0.01,29.99); // сгенерировать новые данные для теста
+    for (size_t i = 0; i < SIGNAL_SIZE; ++i) {
+        source_signal.push_back(rand());
+    }
 
-    SMA<float, float> sma(4, 0.01, 30.99);
+}
 
-    clock_t start = clock();
-    sma.calculate_sma();
-    clock_t end = clock() - start;
-    sma.save_results_to_file(end, (double) end / CLOCKS_PER_SEC);
+vector<float> apply_sma_filter(const vector<float> &source_signal, size_t win_len)
+{
+    if(source_signal.empty())
+        throw invalid_argument("source_signal is empty");
+    if(source_signal.size() < win_len)
+        throw invalid_argument("source_signal.size() < win_len");
 
-    //sma.save_prices_to_file();
+    vector<float> _p;
+    float last_signal = source_signal[0];
+    float sum = 0.0;
+    for (size_t i = 0; i < win_len; ++i) {
+        sum += source_signal[i];
+    }
+    _p.push_back(sum / win_len);
+    for (size_t i = win_len; i < SIGNAL_SIZE; ++i) {
+        sum -= last_signal;
+        sum += source_signal[i];
+        last_signal = source_signal[i];
+        _p.push_back(sum / win_len);
+    }
+    return _p;
+}
+int main() {
 
+    vector<float> source;
+    fill_random(source);
+    //print(source);
+    try {
+        auto answer = apply_sma_filter(source,2);
+        //print(answer);
+    }
+    catch (invalid_argument &e) {
+        cerr << e.what() << endl;
+        return EXIT_FAILURE;
+    }
     return 0;
 }
